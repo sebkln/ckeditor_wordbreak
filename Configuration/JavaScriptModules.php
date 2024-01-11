9d789dd39522793236df7933ac0c0eeed69f27f0
Sebastian Klein <laitnin@gmx.net>
2024-01-11 19:40:17 +0100

    [!!!][TASK] Remove old module specifier

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -7,7 +7,5 @@ return [
     ],
     'imports' => [
         '@sebkln/ckeditor-wordbreak' => 'EXT:ckeditor_wordbreak/Resources/Public/JavaScript/plugin/wordbreak.js',
-	// Deprecated: Will be removed in v2.
-        '@sebkln/ckeditor/wordbreak.js' => 'EXT:ckeditor_wordbreak/Resources/Public/JavaScript/plugin/wordbreak.js',
     ],
 ];
