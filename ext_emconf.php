abdd3ea79380b3fad8186de95ee97d32cdb2093f
Sebastian Klein <laitnin@gmx.net>
2025-01-19 15:06:36 +0100

    [TASK] Release 2.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'CKEditor 5: wordbreak Plugin',
     'description' => 'Adds the <wbr> HTML element to CKEditor 5 in TYPO3. If set in a long word, the browser may break a line (without adding a hyphen character). Useful e.g. for long URLs and file paths.',
-    'version' => '2.1.0',
+    'version' => '2.1.1',
     'state' => 'stable',
     'category' => 'be',
     'author' => 'Sebastian Klein',
